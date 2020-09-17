5ea33d0b7beba82344ac1a5dfc2f6e1c3fe5e2cd
saccha07 <61586968+saccha07@users.noreply.github.com>
2020-09-17 12:43:25 +0530

    Create Applied-material-coding-question-2.cpp

---
+++ Applied-material-coding-question-2.cpp
@@ -0,0 +1,5 @@
+/*
+Applied-material-coding-question-2.cpp
+Description: given one single linked lsit which contain a positive and negative number, write a program to seperate positive and negative number, new list will have positivie number first
+then negative number without altering the seq.
+*/
