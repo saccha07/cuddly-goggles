477defda1f45021fd09fcadf5f67a7513c80d06b
saccha07 <61586968+saccha07@users.noreply.github.com>
2020-09-17 11:45:07 +0530

    Create Applied-material-coding-question-1.cpp

---
+++ Applied-material-coding-question-1.cpp
@@ -0,0 +1,8 @@
+/*
+Applied-material-coding-question-1.cpp
+given a makrs of 20 students for 5 subjects,  which is stored in int array say marks[100], however u assumes the restore of marks, WAP to find the count of failed students. criteria is if marks is 
+less than 40 in any subjects and less than 250 in all subjects than students failed.
+Author:Sachindra
+
+
+*/
