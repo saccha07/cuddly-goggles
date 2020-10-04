4e3a3b3119f0f71c0171c97c16c6e79e7b590737
saccha07 <61586968+saccha07@users.noreply.github.com>
2020-10-04 10:07:09 +0530

    Create Smart-Number-Hackerrank-Solution.cpp

---
+++ Smart-Number-Hackerrank-Solution.cpp
@@ -0,0 +1,14 @@
+/*
+Smart-Number-Hackerrank-Solution.cpp
+Description:Smart number hacker rank solution
+Author: Sachindra
+
+*/
+
+bool is_smart_number(int n) {
+int val=int(sqrt(n);
+if(num/pow(val, 2)==1) 
+return True;
+else
+return False;
+}
