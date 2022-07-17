53180c083af937bada855dca32cc084e2fec07cf
saccha07 <61586968+saccha07@users.noreply.github.com>
2022-07-17 15:22:11 +0530

    Update tree.c

--- tree.c
+++ tree.c
@@ -182,3 +182,37 @@ class Solution
         //code here
     }
 };
+
+
+/*Zig Zag traversal of the tree*/
+vector<int> zigZagTraversal(Node* root) {
+    if(root == NULL){return {  } ; }
+ 
+    vector<int > ans ;
+    queue<Node*> q ;
+    q.push(root) ;
+    bool flag = false ;
+ 
+    while(!q.empty()){
+        int size = q.size() ;
+        vector<int> level ;
+        for(int i=0 ; i < size ; i++){
+            Node* node = q.front() ;
+            q.pop() ;
+            level.push_back(node->data) ;
+ 
+            if(node->left != NULL) {q.push(node->left) ;}
+            if(node->right != NULL) {q.push(node->right) ;}   
+ 
+        }
+        flag = !flag ;  
+        if(flag == false){
+            reverse(level.begin() , level.end()) ;           
+        }
+        for(int i = 0 ; i < level.size() ; i++){
+          ans.push_back(level[i]) ;
+        }
+         
+    }
+    return ans ;
+}
